31e3f4fd612b4803bb5c565c05f168085e106bb7
13ruceYu <bruceyuhongbo@gmail.com>
2023-01-02 00:25:25 +0800

    feat: deploy contract success

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/o9vKr0LRu1NqUG4Lt9DfoNdkknemoHLG
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle')
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/o9vKr0LRu1NqUG4Lt9DfoNdkknemoHLG',
+      accounts: ['dbbb3e5dd3bae7e362ac8063002737fd4d5db4d44d90c99a3ee77d9a23fa13d0']
+    }
+  }
+}
